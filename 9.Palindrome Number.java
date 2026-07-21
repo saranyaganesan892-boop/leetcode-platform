080d6b2dbf1a2f86269806a3adcbf42ac3101111
saranyaganesan892-boop <saranyaganesan892@gmail.com>
2026-07-21 09:10:05 +0530

    Create 9.Palindrome Number.java

---
+++ 9.Palindrome Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Check for overflow
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                (reverse == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return false;
+            }
+
+            reverse = reverse * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reverse;
+    }
+}   
+    
