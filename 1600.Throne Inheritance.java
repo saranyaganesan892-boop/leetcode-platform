ae7fec2a6df165259be3b43cb91a7dde5c7de468
saranyaganesan892-boop <saranyaganesan892@gmail.com>
2026-07-21 09:17:14 +0530

    Create 1600.Throne Inheritance.java

---
+++ 1600.Throne Inheritance.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class ThroneInheritance {
+
+    private String king;
+    private Map<String, List<String>> family;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        family = new HashMap<>();
+        dead = new HashSet<>();
+    }
+
+    public void birth(String parentName, String childName) {
+        family.putIfAbsent(parentName, new ArrayList<>());
+        family.get(parentName).add(childName);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> order = new ArrayList<>();
+        dfs(king, order);
+        return order;
+    }
+
+    private void dfs(String person, List<String> order) {
+        if (!dead.contains(person)) {
+            order.add(person);
+        }
+
+        List<String> children = family.get(person);
+        if (children != null) {
+            for (String child : children) {
+                dfs(child, order);
+            }
+        }
+    }
+}
